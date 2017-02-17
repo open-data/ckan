a1b35be25be54ad91208de860e9ed102f4769973
Jinfei Fan <fanjinfei@hotmail.com>
2017-02-17 14:47:09 -0500

    add default sort in package search

--- ckan/controllers/package.py
+++ ckan/controllers/package.py
@@ -165,7 +165,8 @@ class PackageController(base.BaseController):
 
         c.remove_field = remove_field
 
-        sort_by = request.params.get('sort', None)
+        sort_by = request.params.get('sort', 'metadata_modified desc')
+        request.GET['sort'] = sort_by
         params_nosort = [(k, v) for k, v in params_nopage if k != 'sort']
 
         def _sort_by(fields):
@@ -186,10 +187,7 @@ class PackageController(base.BaseController):
             return search_url(params, package_type)
 
         c.sort_by = _sort_by
-        if not sort_by:
-            c.sort_by_fields = []
-        else:
-            c.sort_by_fields = [field.split()[0]
+        c.sort_by_fields = [field.split()[0]
                                 for field in sort_by.split(',')]
 
         def pager_url(q=None, page=None):
