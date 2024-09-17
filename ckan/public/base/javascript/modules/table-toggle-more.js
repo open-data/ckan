2e3ac22bb82e9264ae3f7a6060a108693b4cc7cb
Jesse Vickery <jesse.vickery@tbs-sct.gc.ca>
2024-09-17 18:16:31 +0000

    fix(i18n): show more table module;
    - Minor fixes for our setup of jquery versions.

--- ckan/public/base/javascript/modules/table-toggle-more.js
+++ ckan/public/base/javascript/modules/table-toggle-more.js
@@ -4,7 +4,11 @@
 this.ckan.module('table-toggle-more', function($) {
   return {
     /* options object can be extended using data-module-* attributes */
-    options: {},
+    options: {
+      // (canada fork only): jquery issues fix
+      showLabel: null,
+      hideLabel: null
+    },
 
     /* Initialises the module setting up elements and event listeners.
      *
@@ -18,12 +22,21 @@ this.ckan.module('table-toggle-more', function($) {
       if (rows) {
         // How much is the colspan?
         var cols = $('thead tr th', this.el).length;
+        // (canada fork only): jquery issues fix
+        let showLabel = this._('Show more');
+        if( this.options.showLabel != null ){
+          showLabel = this.options.showLabel;
+        }
+        let hideLabel = this._('Hide');
+        if( this.options.hideLabel != null ){
+          hideLabel = this.options.hideLabel;
+        }
         var template_more = [
           '<tr class="toggle-show toggle-show-more">',
           '<td colspan="'+cols+'">',
           '<small>',
-          '<a href="javascript:void(0);" class="show-more">' + this._('Show more') + '</a>',
-          '<a href="javascript:void(0);" class="show-less">' + this._('Hide') + '</a>',
+          '<a href="javascript:void(0);" class="show-more">' + showLabel + '</a>',
+          '<a href="javascript:void(0);" class="show-less">' + hideLabel + '</a>',
           '</small>',
           '</td>',
           '</tr>'
@@ -35,21 +48,41 @@ this.ckan.module('table-toggle-more', function($) {
           '</tr>'
         ].join('\n');
 
-       var seperator = $(template_seperator).insertAfter($('.toggle-more:last-child', this.el));
+        var seperator = $(template_seperator).insertAfter($('.toggle-more:last-child', this.el));
         $(template_more).insertAfter(seperator);
-        // (canada fork only): jquery version issues w/ recombinant preview
-        //FIXME: binding not working properly!!!
-        console.log($('table').find('a.show-more'));
-        setTimeout(function(){
-          console.log('WORKING!!!');
-          console.log($('table').find('a.show-more'));
-          $('table').find('a.show-more').off('click.Show');
-          $('table').find('a.show-more').on('click.Show', this._onShowMore);
-          $('table').find('a.show-less').off('click.Hide');
-          $('table').find('a.show-less').on('click.Hide', this._onShowLess);
-        }, 500);
-        // $('.show-more', this.el).on('click', this._onShowMore);
-        // $('.show-less', this.el).on('click', this._onShowLess);
+        // (canada fork only): jquery issues fix
+        function _bindButtons(_moduleElement){
+          $('.show-more', _moduleElement).off('click.Show');
+          $('.show-more', _moduleElement).on('click.Show', function(_event){
+            _event.preventDefault();
+            _moduleElement.removeClass('table-toggle-more').addClass('table-toggle-less');
+          });
+          $('.show-more', _moduleElement).off('keyup.Show');
+          $('.show-more', _moduleElement).on('keyup.Show', function(_event){
+            let keyCode = _event.keyCode ? _event.keyCode : _event.which;
+            // enter key required for a11y
+            if( keyCode == 13 ){
+              _event.preventDefault();
+              _moduleElement.removeClass('table-toggle-more').addClass('table-toggle-less');
+            }
+          });
+          $('.show-less', _moduleElement).off('click.Hide');
+          $('.show-less', _moduleElement).on('click.Hide', function(_event){
+            _event.preventDefault();
+            _moduleElement.removeClass('table-toggle-less').addClass('table-toggle-more');
+          });
+          $('.show-less', _moduleElement).off('keyup.Hide');
+          $('.show-less', _moduleElement).on('keyup.Hide', function(_event){
+            let keyCode = _event.keyCode ? _event.keyCode : _event.which;
+            // enter key required for a11y
+            if( keyCode == 13 ){
+              _event.preventDefault();
+              _moduleElement.removeClass('table-toggle-less').addClass('table-toggle-more');
+            }
+          });
+        }
+        _bindButtons(this.el);
+        setTimeout(_bindButtons, 500, this.el);
       }
     },
 
