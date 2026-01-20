0c33919908c0808814fcceb1d082117041c8e364
Jesse Vickery <jesse.vickery@tbs-sct.gc.ca>
2026-01-20 15:14:16 +0000

    fix(tests): version;
    - Added support for old types of python namespace packaging.

--- ckan/__init__.py
+++ ckan/__init__.py
@@ -1,5 +1,15 @@
 # encoding: utf-8
 
+# (canada fork only): old namespace package
+# TODO: remove after python dependency upgrades...
+try:
+    import pkg_resources
+    # type_ignore_reason: reportAttributeAccessIssue
+    pkg_resources.declare_namespace(__name__)
+except ImportError:
+    import pkgutil
+    __path__ = pkgutil.extend_path(__path__, __name__)
+
 __version__ = "2.10.8"
 
 # The packaging system relies on this import, please do not remove it
