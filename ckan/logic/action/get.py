b31d8ba2bc61fd52172f80b34aa2c7f77cbd2a08
Jesse Vickery <jesse.vickery@tbs-sct.gc.ca>
2026-06-16 16:00:17 -0400

    stash

--- ckan/logic/action/get.py
+++ ckan/logic/action/get.py
@@ -1148,11 +1148,12 @@ def package_show(context: Context, data_dict: DataDict) -> ActionResult.PackageS
                 scheme=configured_parts.scheme)
             current_domain = urlunparse(requesting_parts)
         for res_dict in package_dict['resources']:
-            if res_dict.get('url_type') == 'upload':
-                if res_dict.get('url', '').startswith('/'):
-                    res_dict['url'] = current_domain + res_dict['url']
-                if res_dict.get('original_url', '').startswith('/'):
-                    res_dict['original_url'] = current_domain + res_dict['original_url']  # XLoader field
+            if res_dict.get('url_type') != 'upload':
+                continue
+            if res_dict.get('url', '').startswith('/'):
+                res_dict['url'] = current_domain + res_dict['url']
+            if res_dict.get('original_url', '').startswith('/'):
+                res_dict['original_url'] = current_domain + res_dict['original_url']  # XLoader field
 
     return package_dict
 
